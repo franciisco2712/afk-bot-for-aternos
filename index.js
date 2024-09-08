5936fc4c396b1050b73bd7d37ac5cc8bf7cbcc27
franciisco2712 <julioalbertobarrera1962@gmail.com>
2024-09-07 22:00:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["26318"];
+var host = data["FRANCIISCO2712"];
 var username = data["beniciof2017"]
 var bot = mineflayer.createBot({
   host: COMPASBENY.aternos,
